447de17f667ccd8467ec8881d030feb654f3d326
Yoni Gutmann <100601872+jgutmann19@users.noreply.github.com>
2025-04-16 11:42:12 -0400

    d calc wip

--- RSA/d_calc.sv
+++ RSA/d_calc.sv
@@ -1,46 +1,47 @@
 module d_calc (
-    input logic clk,
-    input logic rst,
     input logic [8:0] e,
     input logic [127:0] phi,
     output logic [127:0] d
 );
-    typedef enum logic [1:0] {
-        START,
-        COMPUTE,
-        RESTART,
-        XXX = 'x
-    } state_t;
+    // typedef enum logic [1:0] {
+    //     START,
+    //     COMPUTE,
+    //     RESTART,
+    //     XXX = 'x
+    // } state_t;
 
     
     
-    // Registers
-    state_t state_r, next_state;
+    // // Registers
+    // state_t state_r, next_state;
 
-    logic [127:0] t_r, next_t;
-    logic [127:0] new_t_r, next_new_t;
-    logic [127:0] r_r, next_r;
-    logic [127:0] new_r_r, next_new_r;
-    logic [127:0] temp_r, next_temp;
+    // logic [127:0] t_r, next_t;
+    // logic [127:0] new_t_r, next_new_t;
+    // logic [127:0] r_r, next_r;
+    // logic [127:0] new_r_r, next_new_r;
+    // logic [127:0] temp_r, next_temp;
 
     // Wires
-    logic [127:0] q;
+    // logic [127:0] q;
 
 
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            state_r <= IDLE;
-            t_r <= '0;
-            new_t_r <= '0;
-            r_r <= '0;
-            new_r <= '0;
-            temp_r <= '0;
-        end else begin
-            state_r <= next_state;
-        end
-    end
+    // always_ff @(posedge clk or posedge rst) begin
+    //     if (rst) begin
+    //         state_r <= IDLE;
+    //         t_r <= '0;
+    //         new_t_r <= '0;
+    //         r_r <= '0;
+    //         new_r <= '0;
+    //         temp_r <= '0;
+    //     end else begin
+    //         state_r <= next_state;
+    //     end
+    // end
 
     always_comb begin
+        // d = e^-1 mod phi
+        d = 1 / ($exp(1) % phi); // Placeholder for the actual calculation
+
         // t = '0;
         // new_t = 128'd1;
         // r = phi;
@@ -66,29 +67,29 @@ module d_calc (
         //     t = t + phi; // Ensure t is positive
         // end
         // d = t; // Assign d
-        case (state_r) 
-            START: begin
-                next_t = 0;
-                next_new_t = 1;
-                next_r = phi;
-                next_new_r = 0;
+        // case (state_r) 
+        //     START: begin
+        //         next_t = 0;
+        //         next_new_t = 1;
+        //         next_r = phi;
+        //         next_new_r = 0;
 
-                next_state = COMPUTE;
-            end
+        //         next_state = COMPUTE;
+        //     end
 
-            COMPUTE: begin
-                if (new_r_r != 0) begin
-                    q = r_r / new_r_r;
-                    // next_temp = ;
-                end else begin
-                    next_state = RESTART;
-                end
-            end
+        //     COMPUTE: begin
+        //         if (new_r_r != 0) begin
+        //             q = r_r / new_r_r;
+        //             // next_temp = ;
+        //         end else begin
+        //             next_state = RESTART;
+        //         end
+        //     end
 
-            RESTART: begin
+        //     RESTART: begin
 
-            end
+        //     end
 
-        endcase
+        // endcase
     end
 endmodule
